69da976a61c4dc14af88ecb00c1c3a415597f842
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2023-08-06 20:01:45 -0300

    Disable Player Input in Pause menu

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -7,17 +7,35 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidBody;
 
+    private PlayerInput _playerInput;
+
     [SerializeField]
     private float _forceModifier = 1.5f;
 
-    private void Awake()
+    private void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void OnEnable()
+    {
+        UIController.OnPause += OnPause;
+    }
+
+    private void OnDisable()
+    {
+        UIController.OnPause -= OnPause;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         float x = context.ReadValue<Vector2>().x * _forceModifier;
         _rigidBody.AddForce(new Vector3(x, -1f, 0f));
+    }
+
+    private void OnPause(bool paused)
+    {
+        _playerInput.enabled = !paused;
     }
 }
